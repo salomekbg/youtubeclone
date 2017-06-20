110238df03144d021a49fdb7768ed1d37979e9ba
SluvsP <salomekbg@gmail.com>
2017-06-20 18:43:59 -0400

    update youtube search and created videolist component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-  import YTSearch from 'youtube-api-search';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyA8RkBaNFvR0_-zg5lNOqAinGwYCNJLKnM';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-})
-
 // Create a new component.  This component should produce some HTML
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos = {this.state.videos} />
       </div>
     );
   }
