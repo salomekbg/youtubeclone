9e3c544e3cae03d86226766a05ce7e0e7e1e9c9a
SluvsP <salomekbg@gmail.com>
2017-06-20 17:08:10 -0400

    change App component from functional to class-based in order to use state to manage API data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+  import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyA8RkBaNFvR0_-zg5lNOqAinGwYCNJLKnM';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+})
+
 // Create a new component.  This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
