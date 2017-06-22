166f23f99c7070b22f68ec7e07f0ea89ac4b667a
SluvsP <salomekbg@gmail.com>
2017-06-22 15:31:50 -0400

    create video_detail and update site functionality to show video when its listing is clicked

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyA8RkBaNFvR0_-zg5lNOqAinGwYCNJLKnM';
 
@@ -11,10 +12,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({videos});
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -22,7 +29,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos = {this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
